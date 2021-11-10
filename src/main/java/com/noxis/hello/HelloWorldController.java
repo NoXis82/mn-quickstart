b4ad9aa20b12a907b1e7fda7a6ea8f9249212c33
Туманов Вячеслав <vn.tumanov@me.com>
2021-11-10 22:07:38 +0300

    application config test

--- src/main/java/com/noxis/hello/HelloWorldController.java
+++ src/main/java/com/noxis/hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.noxis.hello;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -13,13 +14,21 @@ public class HelloWorldController {
 
     // method two
     private final MyService service;
+    private final String helloFromConfig;
 
-    public HelloWorldController(MyService service) {
+    public HelloWorldController(MyService service,
+                                @Property(name = "hello.world.message") String helloFromConfig) {
         this.service = service;
+        this.helloFromConfig = helloFromConfig;
     }
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
         return service.helloFromService();
+    }
+
+    @Get(uri = "/config", produces = MediaType.TEXT_PLAIN)
+    public String helloConfig() {
+        return helloFromConfig;
     }
 }
